e8c10a3360875808e51c674622efd759aca847db
Samet İrkören <sametirkoren@gmail.com>
2020-01-06 17:41:15 +0300

    fetching data from TMDB api to Movie Component

--- src/components/Movie.js
+++ src/components/Movie.js
@@ -10,18 +10,26 @@ import Actor from './elements/Actor';
 import Grid from './elements/Grid';
 import Spinner from './elements/Spinner';
 
-const Movie = ({movieId}) => (
-    <>
-        <Navigation/>
-        <MovieInfo/>
-        <MovieInfoBar/>
-        <Grid>
-            <Actor/>
-        </Grid>
-        <Spinner/>
 
+import {useMovieFetch} from './hooks/useMovieFetch';
 
-    </>
-)
+const Movie = ({movieId}) => {
+
+    const [movie , loading ,error] = useMovieFetch(movieId);
+    console.log(movie);
+
+    return (
+        <>
+            <Navigation/>
+            <MovieInfo/>
+            <MovieInfoBar/>
+            <Grid>
+                <Actor/>
+            </Grid>
+            <Spinner/>
+        </>
+    )
+} 
+   
 
 export default Movie;
