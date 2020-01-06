221a0ceb0739b37d522cf2093bf5894d3c2cbbbe
Samet İrkören <sametirkoren@gmail.com>
2020-01-06 13:33:17 +0300

    StyledComponent Header

--- src/components/elements/Header.js
+++ src/components/elements/Header.js
@@ -1,8 +1,24 @@
 import React from 'react';
 
+
+import RMDBLogo from '../images/reactMovie_logo.png';
+import TMDBLogo from '../images/tmdb_logo.svg';
+
+import {
+    StyledHeader,
+    StyledRMDBLogo , 
+    StyledTMDBLogo
+} from '../styles/StyledHeader';
+
 const Header = () => {
     return (
-        <div>Header</div>
+        <StyledHeader>
+            <div className="header-content">
+                <StyledRMDBLogo src={RMDBLogo} alt="rmdb-logo"/>
+                <StyledTMDBLogo src={TMDBLogo} alt="tmdb-logo"/>
+
+            </div>
+        </StyledHeader>
     )
 }
 export default Header ;
