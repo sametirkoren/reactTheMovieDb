a86eae4d025db2766769d5fc38769bb85d32f684
Samet İrkören <sametirkoren@gmail.com>
2020-01-11 12:00:19 +0300

    bug fix

--- src/config.js
+++ src/config.js
@@ -5,7 +5,7 @@
 const API_URL = 'https://api.themoviedb.org/3/';
 const API_KEY = 'bcea853d51668c75308483221dba68a8';
 
-const SEARCH_BASE_URL = `${API_URL}search/movie?api_key=${API_KEY}&query=&language=tr-TR`;
+const SEARCH_BASE_URL = `${API_URL}search/movie?api_key=${API_KEY}&query=`;
 const POPULAR_BASE_URL = `${API_URL}movie/popular?api_key=${API_KEY}&language=tr-TR`;
 
 const IMAGE_BASE_URL = 'http://image.tmdb.org/t/p/';
